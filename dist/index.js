da3f022318d6481b4ff3efaeecfb178432d01bdd
hoangkianh <hoangkianh31592@gmail.com>
2022-03-02 18:34:02 +0700

    initial commit

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dotoracle-web3-react-torus-connector.cjs.production.min.js')
-} else {
-  module.exports = require('./dotoracle-web3-react-torus-connector.cjs.development.js')
-}
